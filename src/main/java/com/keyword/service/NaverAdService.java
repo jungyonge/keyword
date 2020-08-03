00999f221eace00dd39c555b284c11c326959084
Alphonse <jungyongchoi@coocha.com>
2020-08-03 17:50:54 +0900

    Flux 테스트

--- src/main/java/com/keyword/service/NaverAdService.java
+++ src/main/java/com/keyword/service/NaverAdService.java
@@ -14,6 +14,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -77,8 +80,47 @@ public class NaverAdService {
 //
 //        Collections.sort(removeDupKeyword);
 
-        naverBlogStat(lastKeywordList);
-        return lastKeywordList;
+//        Mono.just("test")
+//                .flatMap(id ->
+//                                Flux.range(0, lastKeywordList.size())
+//                                        .retry(5)
+//                                        .parallel(5)
+//                                        .runOn(Schedulers.newParallel("PAR", 5))
+//                                        .map(i -> {
+//                                            naverBlogStatFlux(lastKeywordList, i);
+//                                            return i;
+//                                        })
+//                                        .then())
+//                .subscribe(i -> log.info("완료 : " + i));
+
+
+//        Flux.range(0, lastKeywordList.size())
+//                .retry(5)
+//                .parallel(5)
+//                .runOn(Schedulers.newParallel("PAR", 5))
+//                .map(i -> {
+//                    naverBlogStatFlux(lastKeywordList, i);
+//                    return i;
+//                })
+//                .subscribe(i -> log.info("완료 : " + i));
+
+        List<RelateKeywordStatModel.Keyword> ttt = new ArrayList<>();
+
+        List<RelateKeywordStatModel.Keyword> test =  Flux.range(0, lastKeywordList.size())
+                .retry(5)
+                .parallel(5)
+                .runOn(Schedulers.newParallel("PAR", 5))
+                .map(i -> {
+                    naverBlogStatFlux(lastKeywordList, i);
+                    return lastKeywordList.get(i);
+                })
+                .sequential()
+                .collectList()
+                .block();
+
+//        naverBlogStatFlux();
+//        naverBlogStat(lastKeywordList);
+        return test;
     }
 
     private List<RelateKeywordStatModel.Keyword> getRelKeywordStat(List<RelateKeywordStatModel.Keyword> keywordList) throws SignatureException, UnsupportedEncodingException, InterruptedException {
@@ -234,12 +276,12 @@ public class NaverAdService {
 //                    }
 //                }
 
-                String apiString = getNaverAPIString("https://openapi.naver.com/v1/search/blog.json?query=",tempKeyword.getRelKeyword());
-                NaverBlogStatModel naverBlogStatModel = gson.fromJson(apiString,NaverBlogStatModel.class);
+                String apiString = getNaverAPIString("https://openapi.naver.com/v1/search/blog.json?query=", tempKeyword.getRelKeyword());
+                NaverBlogStatModel naverBlogStatModel = gson.fromJson(apiString, NaverBlogStatModel.class);
 
-                for(int i = 0 ; i < naverBlogStatModel.getItems().size() ; i++){
+                for (int i = 0; i < naverBlogStatModel.getItems().size(); i++) {
                     NaverBlogStatModel.item tempItem = naverBlogStatModel.getItems().get(i);
-                    if(tempItem.getLink().contains("naver") || tempItem.getLink().contains("blog.me")){
+                    if (tempItem.getLink().contains("naver") || tempItem.getLink().contains("blog.me")) {
                         naverCnt++;
                     } else if (tempItem.getLink().contains("tistory")) {
                         tistoryCnt++;
@@ -264,6 +306,119 @@ public class NaverAdService {
                 e.printStackTrace();
             }
         }
+    }
+
+    public void naverBlogStatFlux(List<RelateKeywordStatModel.Keyword> removeDupKeyword, int x) {
+        Gson gson = new Gson();
+
+        RelateKeywordStatModel.Keyword tempKeyword = removeDupKeyword.get(x);
+        String keyword = tempKeyword.getRelKeyword();
+        Document naverBlogDocument = null;
+        Document naverPCDocument = null;
+        Document naverMobileDocument = null;
+        boolean ban = false;
+        Map<String, Integer> result = new HashMap<String, Integer>();
+
+        int whereBlog = 0;
+        int whereWeb = 0;
+        int whereMobileBlog = 0;
+        int whereMobileWeb = 0;
+        int blogTotalPost = 0;
+        int naverCnt = 0;
+        int tistoryCnt = 0;
+        int elseCnt = 0;
+
+        String naverBlogURL = "https://search.naver.com/search.naver?where=post&sm=tab_jum&query=" + keyword;
+        String naverPCURL = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" + keyword;
+        String naverMobileURL = "https://m.search.naver.com/search.naver?query=" + keyword;
+
+        try {
+
+            naverBlogDocument = Jsoup.connect(naverBlogURL).get();
+            naverPCDocument = Jsoup.connect(naverPCURL).get();
+            naverMobileDocument = Jsoup.connect(naverMobileURL).get();
+
+            Elements elements1 = naverBlogDocument.select("ul#elThumbnailResultArea a.url");// 블로그 url
+            Elements elements2 = naverBlogDocument.select("div#main_pack.main_pack span.title_num");// 블로그 포스팅 개수
+            Elements elements3 = naverPCDocument.select("div#main_pack.main_pack div.section_head h2"); // pc 검색시 블로그 몇번째 있는지 search
+            Elements elements4 = naverMobileDocument.select("div#ct a.api_more"); // 모바일 검색시 블로그 몇번째 있는지 search
+            int element1Size = elements1.size();
+            int element2Size = elements2.size();
+            int element3Size = elements3.size();
+            int element4Size = elements4.size();
+
+            if (element3Size != 0) {
+                for (int b = 0; b < element3Size; b++) {
+                    String find = String.valueOf(elements3.get(b).childNode(0));
+                    if (find.equals("블로그")) {
+                        whereBlog = b + 1;
+                    }
+                    if (find.equals("웹사이트")) {
+                        whereWeb = b + 1;
+                    }
+                }
+            }
+
+            if (element4Size != 0) {
+                for (int b = 0; b < element4Size; b++) {
+                    String find = String.valueOf(elements4.get(b).childNode(0));
+                    if (find.equals("VIEW 더보기")) {
+                        whereMobileBlog = b + 1;
+                    }
+                    if (find.equals(" 더보기")) {
+                        whereMobileWeb = b + 1;
+                    }
+                }
+            }
+
+            if (element1Size != 0 && element2Size != 0) {
+                String str = String.valueOf(elements2.get(0).childNode(0));
+                blogTotalPost = Integer.parseInt(str.substring(6).replaceAll("[^0-9]", ""));
+
+                for (int i = 0; i < element1Size; i++) {
+                    Element element = elements1.get(i);
+                    String blogURL = String.valueOf(element.childNode(0));
+                    if (blogURL.indexOf("naver") > -1 || blogURL.indexOf("blog.me") > -1) {
+                        naverCnt++;
+                    } else if (blogURL.indexOf("tistory") > -1) {
+                        tistoryCnt++;
+                    } else {
+                        elseCnt++;
+                    }
+                }
+            }
+
+//            String apiString = getNaverAPIString("https://openapi.naver.com/v1/search/blog.json?query=", tempKeyword.getRelKeyword());
+//            NaverBlogStatModel naverBlogStatModel = gson.fromJson(apiString, NaverBlogStatModel.class);
+//
+//            for (int i = 0; i < naverBlogStatModel.getItems().size(); i++) {
+//                NaverBlogStatModel.item tempItem = naverBlogStatModel.getItems().get(i);
+//                if (tempItem.getLink().contains("naver") || tempItem.getLink().contains("blog.me")) {
+//                    naverCnt++;
+//                } else if (tempItem.getLink().contains("tistory")) {
+//                    tistoryCnt++;
+//                } else {
+//                    elseCnt++;
+//                }
+//            }
+            tempKeyword.setWhereBlog(whereBlog);
+            tempKeyword.setWhereWeb(whereWeb);
+            tempKeyword.setWhereMobileBlog(whereMobileBlog);
+            tempKeyword.setWhereMobileWeb(whereMobileWeb);
+//            tempKeyword.setBlogTotalPost(naverBlogStatModel.getTotal());
+            tempKeyword.setBlogTotalPost(blogTotalPost);
+            tempKeyword.setNaverCnt(naverCnt);
+            tempKeyword.setTistoryCnt(tistoryCnt);
+            tempKeyword.setElseCnt(elseCnt);
+            log.info(tempKeyword.getRelKeyword());
+
+            Thread.sleep(10);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private String getNaverAPIString(String requestURL, String keyword) {
@@ -294,6 +449,119 @@ public class NaverAdService {
         }
 
         return responseBody.toString();
+    }
+
+
+    public void naverBlogStatFlux(RelateKeywordStatModel.Keyword tempKeyword) {
+        Gson gson = new Gson();
+//           for (RelateKeywordStatModel.Keyword tempKeyword : removeDupKeyword) {
+
+        String keyword = tempKeyword.getRelKeyword();
+        Document naverBlogDocument = null;
+        Document naverPCDocument = null;
+        Document naverMobileDocument = null;
+        boolean ban = false;
+        Map<String, Integer> result = new HashMap<String, Integer>();
+
+        int whereBlog = 0;
+        int whereWeb = 0;
+        int whereMobileBlog = 0;
+        int whereMobileWeb = 0;
+        int blogTotalPost = 0;
+        int naverCnt = 0;
+        int tistoryCnt = 0;
+        int elseCnt = 0;
+
+        //            String naverBlogURL = "https://search.naver.com/search.naver?where=post&sm=tab_jum&query=" + keyword;
+        String naverPCURL = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" + keyword;
+        String naverMobileURL = "https://m.search.naver.com/search.naver?query=" + keyword;
+
+        try {
+
+            //                naverBlogDocument = Jsoup.connect(naverBlogURL).get();
+            //                naverPCDocument = Jsoup.connect(naverPCURL).get();
+            naverMobileDocument = Jsoup.connect(naverMobileURL).get();
+
+            //                Elements elements1 = naverBlogDocument.select("ul#elThumbnailResultArea a.url");// 블로그 url
+            //                Elements elements2 = naverBlogDocument.select("div#main_pack.main_pack span.title_num");// 블로그 포스팅 개수
+            //                Elements elements3 = naverPCDocument.select("div#main_pack.main_pack div.section_head h2"); // pc 검색시 블로그 몇번째 있는지 search
+            Elements elements4 = naverMobileDocument.select("div#ct a.api_more"); // 모바일 검색시 블로그 몇번째 있는지 search
+            //                int element1Size = elements1.size();
+            //                int element2Size = elements2.size();
+            //                int element3Size = elements3.size();
+            int element4Size = elements4.size();
+
+            //                if (element3Size != 0) {
+            //                    for (int b = 0; b < element3Size; b++) {
+            //                        String find = String.valueOf(elements3.get(b).childNode(0));
+            //                        if (find.equals("블로그")) {
+            //                            whereBlog = b + 1;
+            //                        }
+            //                        if (find.equals("웹사이트")) {
+            //                            whereWeb = b + 1;
+            //                        }
+            //                    }
+            //                }
+
+            if (element4Size != 0) {
+                for (int b = 0; b < element4Size; b++) {
+                    String find = String.valueOf(elements4.get(b).childNode(0));
+                    if (find.equals("VIEW 더보기")) {
+                        whereMobileBlog = b + 1;
+                    }
+                    if (find.equals(" 더보기")) {
+                        whereMobileWeb = b + 1;
+                    }
+                }
+            }
+
+            //                if (element1Size != 0 && element2Size != 0) {
+            //                    String str = String.valueOf(elements2.get(0).childNode(0));
+            //                    blogTotalPost = Integer.parseInt(str.substring(6).replaceAll("[^0-9]", ""));
+            //
+            //                    for (int i = 0; i < element1Size; i++) {
+            //                        Element element = elements1.get(i);
+            //                        String blogURL = String.valueOf(element.childNode(0));
+            //                        if (blogURL.indexOf("naver") > -1 || blogURL.indexOf("blog.me") > -1) {
+            //                            naverCnt++;
+            //                        } else if (blogURL.indexOf("tistory") > -1) {
+            //                            tistoryCnt++;
+            //                        } else {
+            //                            elseCnt++;
+            //                        }
+            //                    }
+            //                }
+
+            String apiString = getNaverAPIString("https://openapi.naver.com/v1/search/blog.json?query=", tempKeyword.getRelKeyword());
+            NaverBlogStatModel naverBlogStatModel = gson.fromJson(apiString, NaverBlogStatModel.class);
+
+            for (int i = 0; i < naverBlogStatModel.getItems().size(); i++) {
+                NaverBlogStatModel.item tempItem = naverBlogStatModel.getItems().get(i);
+                if (tempItem.getLink().contains("naver") || tempItem.getLink().contains("blog.me")) {
+                    naverCnt++;
+                } else if (tempItem.getLink().contains("tistory")) {
+                    tistoryCnt++;
+                } else {
+                    elseCnt++;
+                }
+            }
+            tempKeyword.setWhereBlog(whereBlog);
+            tempKeyword.setWhereWeb(whereWeb);
+            tempKeyword.setWhereMobileBlog(whereMobileBlog);
+            tempKeyword.setWhereMobileWeb(whereMobileWeb);
+            tempKeyword.setBlogTotalPost(naverBlogStatModel.getTotal());
+            tempKeyword.setNaverCnt(naverCnt);
+            tempKeyword.setTistoryCnt(tistoryCnt);
+            tempKeyword.setElseCnt(elseCnt);
+            log.info(tempKeyword.getRelKeyword());
+
+            Thread.sleep(10);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//           }
     }
 
 }
